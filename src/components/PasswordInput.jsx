188fca4db50e878d5d9b3a7726eed7d95df3fa62
Chadrick <chadrickalvarez@gmail.com>
2025-05-06 15:16:47 -0400

    Lab

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,15 @@
-// Code PasswordInput Component Here
-
-function PasswordInput (){
-    return(
-        <></>
-    )
-}
-
-export default PasswordInput;
+function PasswordInput() {
+    function handleChange(event) {
+      console.log('Entering password...');
+    }
+  
+    return (
+      <input
+        type="password"
+        onChange={handleChange}
+        aria-label="password-input" // optional but test-friendly
+      />
+    );
+  }
+  
+  export default PasswordInput;  
